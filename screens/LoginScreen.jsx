09b0577d396784ded30b54c306743fa2faa5bbfd
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-26 00:49:06 +0300

    complete the login screen styles and  start functionality

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,12 +1,108 @@
-import { ImageBackground, View, Text } from 'react-native';
+import {
+  ImageBackground,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { styles } from '../styles/LoginScreen.styled';
 import Bg from '../assets/login-bg.jpg';
+import { useState } from 'react';
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+  const [focused, setFocused] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  function togglePassword() {
+    setShowPassword((prevState) => !prevState);
+  }
+
   return (
-    <View style={styles.mainContainer}>
-      <ImageBackground source={Bg} resizeMode='cover' style={styles.image}></ImageBackground>
-    </View>
+    <TouchableWithoutFeedback>
+      <View style={styles.mainContainer}>
+        <ImageBackground source={Bg} resizeMode='cover' style={styles.image}>
+          <KeyboardAvoidingView style={styles.keyboard}>
+            <View style={styles.container}>
+              <Text style={styles.title}>Увійти</Text>
+              <View style={styles.form}>
+                <TextInput
+                  id='email'
+                  value={email}
+                  onChange={onChange}
+                  placeholder='Адреса електронної пошти'
+                  autoCompleteType='email'
+                  autoCapitalize='none'
+                  selectionColor={'#FF6C00'}
+                  onFocus={() => {
+                    setFocused('email');
+                  }}
+                  onBlur={() => {
+                    setFocused('');
+                  }}
+                  style={{
+                    ...styles.input,
+                    borderColor: focused === 'email' ? '#FF6C00' : '#E8E8E8',
+                  }}
+                />
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    type={showPassword ? 'text' : 'password'}
+                    id='password'
+                    value={password}
+                    onChange={onChange}
+                    placeholder='Пароль'
+                    secureTextEntry={showPassword}
+                    autoCompleteType='password'
+                    autoCapitalize='none'
+                    selectionColor={'#FF6C00'}
+                    onFocus={() => {
+                      setFocused('password');
+                    }}
+                    onBlur={() => {
+                      setFocused('');
+                    }}
+                    style={{
+                      ...styles.input,
+                      borderColor: focused === 'password' ? '#FF6C00' : '#E8E8E8',
+                    }}
+                  />
+                  <TouchableOpacity onPress={togglePassword} style={styles.passwordIndicator}>
+                    <Text style={styles.showText}>{showPassword ? 'Показати' : 'Сховати'}</Text>
+                  </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={styles.btn}>
+                  <Text style={styles.BtnText}>Увійти</Text>
+                </TouchableOpacity>
+                <View style={styles.wrapper}>
+                  <Text style={styles.loginText}>Немає акаунту?</Text>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('RegistrationScreen')}
+                    style={styles.loginText}
+                  >
+                    <Text style={styles.loginLink}>Зареєструватися</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
