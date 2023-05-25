20cc6adf07e05c61a4fd41a092a35765c84d6525
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-25 22:47:22 +0300

    create start styles for login screen

---
+++ styles/LoginScreen.styled.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+});
