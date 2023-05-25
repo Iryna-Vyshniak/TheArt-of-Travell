7be5b9efb614471f728543f194820a31dc2b955e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-25 22:48:09 +0300

    add bg

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { ImageBackground, View, Text } from 'react-native';
+import { styles } from '../styles/LoginScreen.styled';
+import Bg from '../assets/login-bg.jpg';
 
 const LoginScreen = () => {
-  return <div>LoginScreen</div>;
+  return (
+    <View style={styles.mainContainer}>
+      <ImageBackground source={Bg} resizeMode='cover' style={styles.image}></ImageBackground>
+    </View>
+  );
 };
 
 export default LoginScreen;
