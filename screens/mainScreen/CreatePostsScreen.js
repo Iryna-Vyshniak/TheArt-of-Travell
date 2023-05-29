ea66c70eb1a76c4e100217a2f6b227afa71ce8a1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-29 21:29:12 +0300

    create posts screen UI

--- screens/mainScreen/CreatePostsScreen.js
+++ screens/mainScreen/CreatePostsScreen.js
@@ -1,10 +1,165 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Keyboard,
+  Pressable,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Platform,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const CreatePostsScreen = () => {
+const CreatePostsScreen = ({ navigation, route }) => {
+  const [keyboardStatus, setKeyboardStatus] = useState(false);
+  const [focused, setFocused] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [formData, setFormData] = useState({
+    image: '',
+    title: '',
+    position: '',
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  });
+
+  const { image, title, position, location } = formData;
+
+  useEffect(() => {
+    if (image && title && position) {
+      setDisabled(false);
+    }
+    if (!image && !title && !position) {
+      setDisabled(true);
+    }
+  }, [image, title, position]);
+
+  const keyboardHide = () => {
+    setKeyboardStatus(false);
+    Keyboard.dismiss();
+  };
+
+  const resetForm = () => {
+    setFormData({
+      image: '',
+      title: '',
+      position: '',
+      location: {
+        latitude: '',
+        longitude: '',
+      },
+    });
+  };
+
+  const handlePublishedPost = () => {
+    keyboardHide();
+    console.log(formData);
+    resetForm();
+    setDisabled(true);
+    navigation.navigate('Posts');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Create Posts</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+          <View
+            style={{
+              ...styles.addImage,
+              paddingBottom: keyboardStatus && Platform.OS == 'android' ? 0 : 12,
+            }}
+          >
+            <View style={styles.addImageContainer}>
+              {image ? <Image style={photo} source={{ uri: image }} /> : null}
+              <Pressable
+                accessibilityLabel={image ? 'Change picture' : 'Add picture'}
+                style={{
+                  ...styles.addImageBtn,
+                  backgroundColor: image ? 'rgba(255, 255, 255, 0.3)' : '#ffffff',
+                }}
+              >
+                {image ? (
+                  <Feather name='camera' size={24} color='#FFFFFF' />
+                ) : (
+                  <Feather name='camera' size={24} color='#BDBDBD' />
+                )}
+              </Pressable>
+            </View>
+            {image ? (
+              <Text style={styles.imageCapture}>Редагувати фото</Text>
+            ) : (
+              <Text style={styles.imageCapture}>Завантажте фото</Text>
+            )}
+          </View>
+
+          <View style={{ ...styles.inputWrapper, marginBottom: 16 }}>
+            <TextInput
+              id='title'
+              value={title}
+              placeholder='Назва...'
+              placeholderTextColor='#BDBDBD'
+              selectionColor={'#FF6C00'}
+              onFocus={() => {
+                setKeyboardStatus(true);
+                setFocused('title');
+              }}
+              onBlur={() => {
+                setFocused('');
+              }}
+              onChangeText={(value) => setFormData((prevState) => ({ ...prevState, title: value }))}
+              style={{
+                ...styles.input,
+                borderBottomColor: focused === 'title' ? '#FF6C00' : '#E8E8E8',
+              }}
+            />
+          </View>
+          <View style={styles.inputWrapper}>
+            <Feather name='map-pin' size={24} color='#BDBDBD' />
+            <TextInput
+              id='position'
+              value={position}
+              placeholder='Місцевість...'
+              placeholderTextColor='#BDBDBD'
+              selectionColor={'#FF6C00'}
+              onFocus={() => {
+                setKeyboardStatus(true);
+                setFocused('position');
+              }}
+              onBlur={() => {
+                setFocused('');
+              }}
+              onChangeText={(value) =>
+                setFormData((prevState) => ({ ...prevState, position: value }))
+              }
+              style={{
+                ...styles.input,
+                borderBottomColor: focused === 'position' ? '#FF6C00' : '#E8E8E8',
+              }}
+            />
+          </View>
+
+          <TouchableOpacity
+            onPress={handlePublishedPost}
+            disabled={disabled}
+            style={{ ...styles.formBtn, backgroundColor: disabled ? '#F6F6F6' : '#FF6C00' }}
+          >
+            <Text
+              style={disabled ? { ...styles.formBtnText, color: '#BDBDBD' } : styles.formBtnText}
+            >
+              Опубліковати
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteBtn} onPress={resetForm}>
+            <Feather name='trash-2' size={24} color='#BDBDBD' />
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -13,8 +168,107 @@ export default CreatePostsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 16,
+  },
+
+  keyboard: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+
+    height: '100%',
+    width: '100%',
+  },
+
+  // camera
+  addImage: {
+    position: 'relative',
+    marginTop: 32,
+    alignItems: 'center',
+  },
+  addImageContainer: {
+    width: '100%',
+    height: 240,
+    backgroundColor: '#F6F6F6',
+    borderColor: '#E8E8E8',
+    borderWidth: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  photo: {
+    width: 343,
+    height: 240,
+    borderWidth: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  addImageBtn: {
+    position: 'absolute',
+    transform: [{ translateX: 145 }, { translateY: 90 }],
+    padding: 18,
+    borderRadius: 50,
+  },
+  imageCapture: {
+    marginTop: 8,
+    marginBottom: 32,
+    alignSelf: 'flex-start',
+    color: '#BDBDBD',
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: 'left',
+  },
+  // input
+  inputWrapper: {
+    marginBottom: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'transparent',
+    borderBottomColor: '#E8E8E8',
+    backgroundColor: 'transparent',
+  },
+
+  input: {
+    textAlign: 'left',
+    width: '100%',
+    color: '#212121',
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  // button
+  formBtn: {
+    padding: 16,
+    borderRadius: 100,
+    marginTop: 32,
+    width: 343,
+  },
+
+  formBtnText: {
+    color: '#FFFFFF',
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  // trash
+  deleteBtn: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 120,
+    width: 70,
+    height: 40,
+    backgroundColor: '#F6F6F6',
+    borderRadius: 20,
   },
 });
+
+// <Feather name="arrow-up" size={24} color="#FFFFFF" />
