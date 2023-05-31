2b6f30abdaa16a121e3db11ea694d84f35eb1be7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-31 21:19:41 +0300

    update

--- screens/Navigation.js
+++ screens/Navigation.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import useRoute from '../router';
-//import useRoute from '../routerAsDesign';
+//import useRoute from '../router';
+import useRoute from '../routerAsDesign';
 
 const Navigation = () => {
   const routing = useRoute({});
