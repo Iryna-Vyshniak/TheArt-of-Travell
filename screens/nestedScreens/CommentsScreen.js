509c1cf90a0766c8bdb27f081f0eed03701a97f4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-01 20:06:30 +0300

    create start comments screen UI

--- screens/nestedScreens/CommentsScreen.js
+++ screens/nestedScreens/CommentsScreen.js
@@ -1,9 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useLayoutEffect } from 'react';
+import { View, Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
-const CommentsScreen = () => {
+//import { Feather } from '@expo/vector-icons';
+//  <Feather name='arrow-up' size={24} color='#FFFFFF' />;
+
+const CommentsScreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: 'Коментарі' });
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-      <Text>Comments</Text>
+      <TouchableWithoutFeedback>
+        <View style={styles.imageWrapper}>
+          <Image
+            style={styles.postImage}
+            source={{
+              uri: 'https://images.unsplash.com/photo-1592859600972-1b0834d83747?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
+            }}
+          />
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
@@ -13,8 +30,19 @@ export default CommentsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    paddingBottom: 16,
+  },
+  imageWrapper: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  postImage: {
+    width: 343,
+    height: 240,
+    borderRadius: 8,
   },
 });
