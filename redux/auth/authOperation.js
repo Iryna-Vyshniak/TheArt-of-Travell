bea747980d657020f6bbe3c78dabb8662b666d34
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-05 21:04:07 +0300

    update profile, refactor

--- redux/auth/authOperation.js
+++ redux/auth/authOperation.js
@@ -1,3 +1,4 @@
+import { Alert } from 'react-native';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -6,16 +7,30 @@ import {
 } from 'firebase/auth';
 import { auth } from '../../firebase/config';
 import { authSlice } from './authReducer';
+const { updateUserProfile, authStateChange, authLogOut } = authSlice.actions;
 
 export const authSignUpUser =
   ({ email, password, name }) =>
   async (dispatch, getState) => {
     console.log('FORMDATA ===>', email, password, name);
     try {
-      const { user } = await createUserWithEmailAndPassword(auth, email, password);
-      dispatch(authSlice.actions.updateUserProfile({ userId: user.uid }));
-      console.log('User: ', user);
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+
+      await updateProfile(user, { displayName: name });
+
+      const { uid, displayName } = user;
+      console.log(displayName, uid);
+
+      dispatch(
+        updateUserProfile({
+          userId: uid,
+          name: displayName,
+          email,
+        })
+      );
     } catch (error) {
+      Alert.alert(error.message);
       throw error;
     }
   };
@@ -23,11 +38,34 @@ export const authSignUpUser =
 export const authSignInUser =
   ({ email, password }) =>
   async (dispatch, getState) => {
-    console.log('FORMDATA ===>', email, password);
+    console.log('credential: ', email, password);
     try {
-      const user = await signInWithEmailAndPassword(auth, email, password);
-      console.log('USER: ', user);
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      const { uid, displayName } = user;
+      console.log(displayName, uid);
+
+      dispatch(
+        updateUserProfile({
+          userId: uid,
+          name: displayName,
+          //email: email,
+        })
+      );
     } catch (error) {
+      Alert.alert("Error! Email or password doesn't match");
       throw error;
     }
   };
+
+export const authStateChangeUser = () => async (dispatch, getState) => {
+  console.log('FORMDATA ===>', email, password);
+  try {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    console.log('USER: ', user);
+  } catch (error) {
+    Alert.alert(error.message);
+    throw error;
+  }
+};
