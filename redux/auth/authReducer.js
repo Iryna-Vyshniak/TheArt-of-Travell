fb89890a9e6f266122a42de55bce8fd9d9f6dc6a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-06 20:01:50 +0300

    refactor

--- redux/auth/authReducer.js
+++ redux/auth/authReducer.js
@@ -7,19 +7,19 @@ const state = {
   stateChange: false,
 };
 
+const actions = {
+  updateUserProfile: (state, { payload }) => ({
+    ...state,
+    userId: payload.userId,
+    name: payload.name,
+    email: payload.email,
+  }),
+  authStateChange: (state, { payload }) => ({ ...state, stateChange: payload.stateChange }),
+  authSignOut: () => state,
+};
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: state,
-  reducers: {
-    updateUserProfile: (state, { payload }) => ({
-      ...state,
-      userId: payload.userId,
-      name: payload.name,
-      email: payload.email,
-    }),
-    authStateChange: (state, { payload }) => ({ ...state, stateChange: payload.stateChange }),
-  },
-  authSignOut: () => state,
+  reducers: actions,
 });
-
-console.log(authSlice.actions);
