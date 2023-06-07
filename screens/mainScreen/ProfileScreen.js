ad43242ffd4eedf6da38b6f6c9072ce0cfd84904
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-07 22:01:34 +0300

    create profile screen UI and star dynamic functionality to filtering posts

--- screens/mainScreen/ProfileScreen.js
+++ screens/mainScreen/ProfileScreen.js
@@ -1,17 +1,46 @@
-import { SafeAreaView, ImageBackground, View, Image, Pressable, Text, Alert } from 'react-native';
+import {
+  SafeAreaView,
+  ImageBackground,
+  View,
+  Image,
+  Pressable,
+  Text,
+  Alert,
+  FlatList,
+} from 'react-native';
 import { StyleSheet } from 'react-native';
 import Bg from '../../assets/login-bg.jpg';
 import Icon from '@expo/vector-icons/Feather';
 
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { authSignOutUser } from '../../redux/auth/authOperation';
 import { db } from '../../firebase/config';
+import { collection, query, onSnapshot, where, getDocs } from 'firebase/firestore';
 
 // import BtnLogOut from '../../components/BtnLogOut';
 
 const ProfileScreen = ({ navigation }) => {
   const [userAvatar, setUserAvatar] = useState(null);
+
+  const [userPosts, setUserPosts] = useState([]);
+  const { userId, name, avatarImage } = useSelector((state) => state.auth);
+
+  const getUserPosts = async () => {
+    try {
+      const postRef = query(collection(db, 'posts'), where('userId', '==', `${userId}`));
+      onSnapshot(postRef, (snapshot) => {
+        setUserPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getUserPosts();
+  }, []);
 
   const dispatch = useDispatch();
 
@@ -29,11 +58,11 @@ const ProfileScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.mainContainer}>
-      <ImageBackground source={Bg} resizeMode='cover' style={styles.image}>
+      <ImageBackground source={Bg} style={styles.imageBg}>
         <View style={styles.container}>
-          <View style={styles.box}>
+          <View style={styles.boxProfile}>
             {userAvatar ? (
-              <Image source={require('../../assets/avatar.png')} resizeMode='cover' />
+              <Image source={require('../../assets/avatar.png')} style={styles.avatarImage} />
             ) : null}
             <Pressable
               onPress={userAvatar ? handleRemoveAvatar : handleAddAvatar}
@@ -55,47 +84,68 @@ const ProfileScreen = ({ navigation }) => {
               )}
             </Pressable>
           </View>
-          {/* <BtnLogOut /> */}
           <Pressable style={styles.logoutBtn} onPress={signOut}>
             <Icon name='log-out' size={24} color='#BDBDBD' />
           </Pressable>
-          <Text style={styles.name}>Natali Romanova</Text>
-          <View style={styles.card}>
-            <View style={styles.imageThumb}>
-              <Image
-                source={{
-                  uri: 'https://images.unsplash.com/photo-1592859600972-1b0834d83747?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
-                }}
-                style={{ ...styles.picture, width: 343, height: 240 }}
-              />
+          <Text style={styles.name}>{name}</Text>
+
+          {!userPosts && (
+            <View style={styles.emptyBox}>
+              <Text style={styles.noPosts}>У Вас немає постів</Text>
             </View>
-            <Text style={styles.imageTitle}>Ліс</Text>
-            <View style={styles.wrapperData}>
-              <View style={styles.feedbackWrapper}>
-                <Pressable onPress={() => navigation.navigate('Comments')} style={styles.feedback}>
-                  <Icon
-                    name='message-circle'
-                    size={24}
-                    color='#FF6C00'
-                    style={{ transform: [{ rotate: '-90deg' }] }}
-                  />
-                  <Text style={styles.feedbackCounter}>8</Text>
-                </Pressable>
-                <Pressable style={styles.feedback}>
-                  <Icon name='thumbs-up' size={24} color='#FF6C00' />
-                  <Text style={styles.feedbackCounter}>153</Text>
-                </Pressable>
-              </View>
-              <Pressable
-                onPress={() => Alert.alert('', 'Link to future map')}
-                style={styles.location}
-              >
-                {/* <Pressable onPress={() => navigation.navigate('Map')} style={styles.location}> */}
-                <Icon name='map-pin' size={24} color='#BDBDBD' />
-                <Text style={styles.locationTitle}>Ukraine</Text>
-              </Pressable>
-            </View>
-          </View>
+          )}
+
+          {userPosts && (
+            <FlatList
+              keyExtractor={(item) => item.id}
+              data={userPosts}
+              renderItem={({ item }) => (
+                <View style={styles.card}>
+                  <View style={styles.imageThumb}>
+                    <Image
+                      source={{
+                        uri: item.photo,
+                      }}
+                      style={{ ...styles.picture, width: 343, height: 240 }}
+                    />
+                  </View>
+
+                  <Text style={styles.imageTitle}>{item.title}</Text>
+                  <View style={styles.wrapperData}>
+                    <View style={styles.feedbackWrapper}>
+                      <Pressable
+                        onPress={() => navigation.navigate('Comments', { postId: item.id })}
+                        style={styles.feedback}
+                      >
+                        <Icon
+                          name='message-circle'
+                          size={24}
+                          color='#FF6C00'
+                          style={{ transform: [{ rotate: '-90deg' }] }}
+                        />
+                        <Text style={styles.feedbackCounter}>8</Text>
+                      </Pressable>
+                      <Pressable style={styles.feedback}>
+                        <Icon name='thumbs-up' size={24} color='#FF6C00' />
+                        <Text style={styles.feedbackCounter}>153</Text>
+                      </Pressable>
+                    </View>
+                    {/* <Pressable
+                    onPress={() => Alert.alert('', 'Link to future map')}
+                    style={styles.location}
+                  > */}
+                    <Pressable
+                      onPress={() => navigation.navigate('Map', { location: item.location })}
+                      style={styles.location}
+                    >
+                      <Icon name='map-pin' size={24} color='#BDBDBD' />
+                      <Text style={styles.locationTitle}>{item.position}</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              )}
+            />
+          )}
         </View>
       </ImageBackground>
     </SafeAreaView>
@@ -110,20 +160,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
   },
-  image: {
+  imageBg: {
     flex: 1,
-    justifyContent: 'flex-end',
+    resizeMode: 'cover',
   },
-  container: {
+  emptyBox: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 43,
-    width: '100%',
+    padding: 16,
+    height: 240,
+  },
+  noPosts: {
+    marginTop: -32,
+    marginBottom: 32,
+    color: '#212121',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'center',
+  },
+  // header
+  container: {
+    position: 'relative',
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 147,
+    paddingHorizontal: 16,
+
+    backgroundColor: '#FFFFFF',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    backgroundColor: '#fff',
   },
   // avatar
-  box: {
+  boxProfile: {
     position: 'relative',
     top: -60,
     alignSelf: 'center',
@@ -153,6 +224,12 @@ const styles = StyleSheet.create({
     top: 22,
     right: 18,
   },
+  avatarImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 16,
+    resizeMode: 'cover',
+  },
   // name
   name: {
     marginTop: -32,
@@ -168,6 +245,7 @@ const styles = StyleSheet.create({
     marginBottom: 32,
     width: 343,
     height: 299,
+    backgroundColor: '#FFFFFF',
   },
   imageThumb: {
     marginBottom: 8,
@@ -176,6 +254,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
   picture: {
+    resizeMode: 'cover',
     borderRadius: 8,
   },
 
@@ -194,6 +273,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'row',
+    width: 343,
   },
   feedbackWrapper: {
     display: 'flex',
