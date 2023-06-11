dd223598faf80aa11c290dbb1fe8614bc568bdeb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-12 00:00:39 +0300

    create

---
+++ shared/theme/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  light: {
+    theme: 'light',
+    color: '#212121',
+    background: '#FFFFFF',
+    box: 'rgba(0, 0, 0, 0.03)',
+  },
+  dark: {
+    theme: 'dark',
+    color: '#FFFFFF',
+    background: '#000000',
+    box: '#696969',
+  },
+};
