cafe5f142aebc79dbacf5a41fa222d02fa0f87b6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-12 17:02:43 +0300

    add colors

--- shared/theme/theme.js
+++ shared/theme/theme.js
@@ -4,11 +4,15 @@ export const theme = {
     color: '#212121',
     background: '#FFFFFF',
     box: 'rgba(0, 0, 0, 0.03)',
+    shadow: '#000000',
+    tab: '#FFFFFF',
   },
   dark: {
     theme: 'dark',
     color: '#FFFFFF',
     background: '#000000',
     box: '#696969',
+    shadow: '#FF6C00',
+    tab: '#4d0000',
   },
 };
