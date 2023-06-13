116cce99347d528039fb48718533a1acbd07deac
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-13 22:22:26 +0300

    add bg for profile

---
+++ src/shared/theme/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  light: {
+    theme: 'light',
+    color: '#212121',
+    background: '#FFFFFF',
+    box: 'rgba(0, 0, 0, 0.03)',
+    shadow: '#000000',
+    tab: '#FFFFFF',
+    profile: '#FFFFFF',
+  },
+  dark: {
+    theme: 'dark',
+    color: '#FFFFFF',
+    background: '#000000',
+    box: '#696969',
+    shadow: '#FF6C00',
+    tab: '#4d0000',
+    profile: '#2b1212',
+  },
+};
